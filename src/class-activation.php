0a9e910e19454500f91333c0468e1033826928f7
Lisa Schuyler <lisa@automattic.com>
2022-05-26 09:58:47 -0700

    Fix version numbering

--- src/class-activation.php
+++ src/class-activation.php
@@ -13,7 +13,7 @@ namespace InactiveAccount;
 /**
  * Activation class.
  *
- * @since   0.1
+ * @since   0.1.0
  *
  * @package InactiveAccount
  * @author  Lisa Schuyler
@@ -25,20 +25,19 @@ class Activation {
 	 *
 	 * @since 0.1.0
 	 *
-	 * @var offset
 	 */
 	public static function activate() {
 
-		// on plugin activation, register the new role, if it doesn't already exist
+		// on plugin activation, register the new role, if it doesn't already exist.
 		add_role( 'inactive', 'Inactive User', [] );
 
-		// schedule job to find and flag inactive accounts
+		// schedule job to find and flag inactive accounts.
 		if ( ! wp_next_scheduled( 'flag_inactive_accounts' ) ) {
 			wp_schedule_event( time(), 'daily', 'flag_inactive_accounts' );
 		}
 
 		$offset = new Calculate_Offset();
-		// schedule a one-time job to flag inactive users who don't log in during the first 90 days
+		// schedule a one-time job to flag inactive users who don't log in during the first 90 days.
 		if ( ! wp_next_scheduled( 'onetime_90day_mark_inactive' ) ) {
 			wp_schedule_single_event( $offset->get_cutoff_timestamp(), 'onetime_90day_mark_inactive' );
 		}
